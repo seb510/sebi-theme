bafe9b5450f39dd7f78d88cf4d202ce37a67d1c9
shurshyn-sergey <Sergey.Shurshyn@gmail.com>
2024-07-16 12:02:28 +0300

    optimize ajax request

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,31 +10,24 @@ jQuery(document).ready(function ($) {
             this.search_name = '';
             this.pPp = this.contentBlock.attr('data-ppage');
             this.category = this.contentBlock.attr('data-category');
+            this.my_search = [];
             this.addListener();
         }
 
         addListener() {
-            let self = this;
-            this.loadMoreBtn.on('click', function (e) {
+            this.loadMoreBtn.on('click', (e) => {
                 e.preventDefault();
-                self.loadMore();
-            });
-            $('#sorting-post').on('change', function () {
-                let current_sort = this.value;
-                self.filterPost(current_sort);
+                this.loadMore();
             });
 
-            const my_search = [];
-            $(document).on('keyup', '#search-name', function (e) {
-                let input_value = $(this).val();
-                my_search.map(e => {
-                    clearTimeout(e)
-                })
-                my_search.push(setTimeout(() => {
-                    self.search_name = input_value.toLowerCase();
-                    self.submitRequest('filter')
-                }, 1000))
-            })
+            $('#sorting-post').on('change', (e) => {
+                this.filterPost(e.target.value);
+            });
+
+            $(document).on('keyup', '#search-name', (e) => {
+                let input_value = $(e.target).val();
+                this.handleSearch(input_value);
+            });
         }
 
         loadMore() {
@@ -45,76 +38,101 @@ jQuery(document).ready(function ($) {
         filterPost(sortInput) {
             this.currentPage = 1;
 
-            if (sortInput === 'date-asc') {
-                this.sortby = 'date';
-                this.sort = 'ASC';
-            } else if (sortInput === 'date-desc') {
-                this.sortby = 'date';
-                this.sort = 'DESC';
-            } else if (sortInput === 'title-asc') {
-                this.sortby = 'title';
-                this.sort = 'ASC';
-            } else if (sortInput === 'title-desc') {
-                this.sortby = 'title';
-                this.sort = 'DESC';
-            } else {
-                this.sortby = 'date';
-                this.sort = 'DESC';
-            }
+            const sortOptions = {
+                'date-asc': { sortby: 'date', sort: 'ASC' },
+                'date-desc': { sortby: 'date', sort: 'DESC' },
+                'title-asc': { sortby: 'title', sort: 'ASC' },
+                'title-desc': { sortby: 'title', sort: 'DESC' }
+            };
+
+            const { sortby, sort } = sortOptions[sortInput] || { sortby: 'date', sort: 'DESC' };
+            this.sortby = sortby;
+            this.sort = sort;
 
             this.submitRequest('filter');
         }
 
+        handleSearch(inputValue) {
+            this.my_search.forEach(clearTimeout);
+            this.my_search.push(setTimeout(() => {
+                this.search_name = inputValue.toLowerCase();
+                this.submitRequest('filter');
+            }, 1000));
+        }
+
         submitRequest(current_request) {
-            let self = this;
             $.ajax({
                 type: 'POST',
                 url: my_ajax_object.ajax_url,
                 dataType: 'json',
                 data: {
                     action: 'get_ajax_posts',
-                    paged: self.currentPage,
-                    ppp: self.pPp,
-                    category: self.category,
-                    sortby: self.sortby,
-                    sort: self.sort,
-                    search: self.search_name,
+                    paged: this.currentPage,
+                    ppp: this.pPp,
+                    category: this.category,
+                    sortby: this.sortby,
+                    sort: this.sort,
+                    search: this.search_name,
                 },
-                beforeSend: function () {
-                    self.contentBlock.addClass('loading');
+                beforeSend: () => {
+                    this.contentBlock.addClass('loading');
                 },
-                success: function (res) {
-                    let data = res.data;
-                    self.contentBlock.removeClass('loading');
-                    if (self.currentPage >= data.max_pages) {
-                        self.loadMoreBtn.hide();
-                    } else {
-                        self.loadMoreBtn.show();
-                    }
-                    if (current_request === 'filter') {
-                        self.contentBlock.html(data.output);
-                    } else {
-                        self.contentBlock.append(data.output);
-                    }
+                success: (res) => {
+                    this.handleSuccess(res, current_request);
                 },
-                error: function (jqXHR, exception) {
-                    if (jqXHR.status === 0) {
-                        console.log('Not connect. Verify Network.');
-                    } else if (jqXHR.status == 404) {
-                        console.log('Requested page not found (404).');
-                    } else if (jqXHR.status == 500) {
-                        console.log('Internal Server Error (500).');
-                    } else if (exception === 'parsererror') {
-                        console.log('Requested JSON parse failed.');
-                    } else if (exception === 'timeout') {
-                        console.log('Time out error.');
-                    } else if (exception === 'abort') {
-                        console.log('Ajax request aborted.');
-                    } else {
-                        console.log('Uncaught Error. ' + jqXHR.responseText);
-                    }
+                error: (jqXHR, exception) => {
+                    this.handleError(jqXHR, exception);
                 }
             });
+        }
+
+        handleSuccess(res, current_request) {
+            const data = res.data;
+            this.contentBlock.removeClass('loading');
+
+            if (this.currentPage >= data.max_pages) {
+                this.loadMoreBtn.hide();
+            } else {
+                this.loadMoreBtn.show();
+            }
+
+            if (current_request === 'filter') {
+                this.contentBlock.html(data.output);
+            } else {
+                this.contentBlock.append(data.output);
+            }
+        }
+
+        handleError(jqXHR, exception) {
+            let errorMsg;
+
+            switch (jqXHR.status) {
+                case 0:
+                    errorMsg = 'Not connect. Verify Network.';
+                    break;
+                case 404:
+                    errorMsg = 'Requested page not found (404).';
+                    break;
+                case 500:
+                    errorMsg = 'Internal Server Error (500).';
+                    break;
+                default:
+                    errorMsg = this.getErrorByException(exception, jqXHR.responseText);
+                    break;
+            }
+
+            console.log(errorMsg);
+        }
+
+        getErrorByException(exception, responseText) {
+            const errorMessages = {
+                'parsererror': 'Requested JSON parse failed.',
+                'timeout': 'Time out error.',
+                'abort': 'Ajax request aborted.',
+                'default': `Uncaught Error. ${responseText}`
+            };
+
+            return errorMessages[exception] || errorMessages['default'];
         }
     }
     class ContactForm {
