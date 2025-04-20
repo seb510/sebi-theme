de878598da732c5c609fdfac24e7b38944db6276
seb510 <sebistian@ukr.net>
2025-04-20 17:34:22 +0300

    upate menu

--- inc/menu.php
+++ inc/menu.php
@@ -3,5 +3,6 @@
  * Register Navigation Menu
  */
 register_nav_menus(array(
-    'header-menu'    => __('Header Menu', THEME_NAME)
+    'header-menu'    => __('Header Menu', THEME_NAME),
+    'footer-menu'    => __('Footer Menu', THEME_NAME)
 ));
